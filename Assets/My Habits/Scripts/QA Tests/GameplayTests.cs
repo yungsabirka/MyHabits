f7d5b8254fc7c0adc01682a84c4958d6ec79c045
Sabir Saiadov <sabirsayadov18@gmail.com>
2024-09-23 17:47:57 +0300

    Changed some tests

--- Assets/My Habits/Scripts/QA Tests/GameplayTests.cs
+++ Assets/My Habits/Scripts/QA Tests/GameplayTests.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GameplayTests : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartTests()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/My Habits/Scripts/QA Tests/UITests.cs
+++ Assets/My Habits/Scripts/QA Tests/UITests.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class UITests : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void StartTests()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
